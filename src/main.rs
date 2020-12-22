9ff618b92b3c3ba86452c9852720b4c65f9aa3c2
Michael Poirier <mikepoirier26@gmail.com>
2020-12-21 20:29:25 -0700

    Setup hello world http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
+
+async fn echo(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    Ok(Response::new(Body::from("Hello, World!")))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 8080).into();
+
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
+
+    let server = Server::bind(&addr).serve(service);
+
+    println!("Address: {}", addr);
+
+    server.await?;
+
+    Ok(())
 }
