8fcb845523331b2a5c09e6101755304dd1273c00
Michael Poirier <mikepoirier26@gmail.com>
2020-12-21 21:54:40 -0700

    Add gracefull shutdown

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,26 @@ use hyper::{
     Body, Request, Response, Server,
 };
 
-async fn echo(_: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    Ok(Response::new(Body::from("Hello, World!")))
+async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let method = req.method();
+    let path = req.uri().path();
+
+    Ok(Response::new(Body::from(format!(
+        "Method: {}, Path: {}",
+        method, path
+    ))))
+}
+
+async fn shutdown_signal() {
+    let shutdown = tokio::signal::ctrl_c().await;
+    match shutdown {
+        Ok(_) => println!("Gracefully shutting down"),
+        Err(e) => eprintln!("Server Error: {}", e),
+    };
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+async fn main() {
     let addr = ([127, 0, 0, 1], 8080).into();
 
     let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
@@ -17,7 +31,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     println!("Address: {}", addr);
 
-    server.await?;
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
 
-    Ok(())
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
+    }
 }
